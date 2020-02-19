7bc9bfccc4f8ab072cfe330925918dab654527c9
Gael Pasgrimaud <gael@gawel.org>
2020-02-19 10:14:54 +0100

    devine

---
+++ README.md
@@ -0,0 +1,6 @@
+
+https://www.hackinscience.org/exercises/
+
+https://github.com/exercism/python/tree/master/exercises/
+
+https://gitlab.com/e2li/cours-python/-/tree/master/saison-1/sources/
---
+++ exercices/devine_nombre.py
@@ -0,0 +1,17 @@
+import random
+
+secret = random.randint(0, 100)
+
+print("Devine le nombre auquel je pense")
+while True:
+    response = input("votre reponse: ")
+    response = int(response)
+
+    if response == secret:
+        print("Gagné")
+        break
+    else:
+        if response > secret:
+            print("Trop grand")
+        if response < secret:
+            print("Trop petit")
